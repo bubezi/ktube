2853e81ac8493aea7aa9eba3d81d4c986647e2eb
Blacking-highness <pbubezi@gmail.com>
2023-06-11 01:50:04 +0300

    update

--- ktube/gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-import multiprocessing
-
-bind = "127.0.0.1:8000"  # Replace with the appropriate bind address for your application
-workers = multiprocessing.cpu_count() * 2 + 1  # Adjust the number of workers as per your system's configuration
-# Add other Gunicorn options as needed
