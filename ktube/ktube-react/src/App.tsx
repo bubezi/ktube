5aa34f434c14e3f96ef5b129c5914d04281e2dd4
Blacking-highness <pbubezi@gmail.com>
2023-07-11 09:09:10 +0300

    update

--- ktube/ktube-react/src/App.tsx
+++ ktube/ktube-react/src/App.tsx
@@ -1,10 +1,8 @@
-// import './App.css'
+import './App.css'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import { Routes, Route, Link } from "react-router-dom";
 import Homepage from './components/Homepage';
-// import Watchpage from "./components/Watchpage";
 import Navbar from "./components/Navbar";
 
 
@@ -13,14 +11,6 @@ function App() {
   return (
     <>
     <div className="App">
-      {/* <nav className="navbar"> */}
-        {/* <Link to="/" className="nav-item">Homepage</Link> */}
-        {/* <Link to="/" className="nav-item">Watchpage</Link> */}
-      {/* </nav> */}
-      {/* <Routes> */}
-        {/* <Route path="/watch" element={<Watchpage/>}/> */}
-        {/* <Route path="/" element={<Homepage/>}/> */}
-      {/* </Routes> */}
       <Navbar username="Bubezi"/>
       <Homepage/>
     </div>
