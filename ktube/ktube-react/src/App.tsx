ca221e8153e757926d36438fc8120500b6daee0a
Blacking-highness <pbubezi@gmail.com>
2023-07-12 22:52:42 +0300

    update

--- ktube/ktube-react/src/App.tsx
+++ ktube/ktube-react/src/App.tsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Homepage from './components/Homepage';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <div className="container">
         <Homepage/>
       </div>
+      <Footer/>
     </div>
     </>
   )
