3eeaa0a672dbea9f5cbe89c4ca7fc946065246fc
Blacking-highness <pbubezi@gmail.com>
2023-07-16 04:12:09 +0300

    homepage complete

--- ktube/ktube-react/src/components/Videocard.tsx
+++ ktube/ktube-react/src/components/Videocard.tsx
@@ -1,5 +1,5 @@
 // import placeholder from '../assets/images/thumbnail-placeholder.webp'
-// import imagePlaceholder from '../assets/images/placeholder.png'
+import imagePlaceholder from '../assets/images/placeholder.png'
 import axios from 'axios'
 import { API_URL } from '../constants'
 import React from 'react'
@@ -23,13 +23,12 @@ function Videocard (props: Prop) {
     const channelDetailsState: ChannelDetailsState = {profile_picture:'', name:''};
     const [channelDetails, setChannelDetails] = React.useState(channelDetailsState);
 
+
     axios.get(API_URL+"dp/"+props.channelId)
         .then(res => setChannelDetails(res.data));
 
     const channelDp = `${channelDetails.profile_picture}`;
     const videoChannelName = `${channelDetails.name}`;
-
-    const pluralViews = props.views === 1 ? 'view' : 'views';
 
 
     const videoTitle = {
@@ -40,13 +39,33 @@ function Videocard (props: Prop) {
         width: "20px", 
         height: "20px",
     }
-
+    
     const priceStyle = {
         // float: "right", 
         color: "green",
         marginBottom: "0 !important",
     }
+    
 
+    const pluralViews = props.views === 1 ? 'view' : 'views';
+
+    function Thumbnail () {
+        if (props.thumbnail===''){
+            return ( <img src={imagePlaceholder} className="channel-icon" alt="Channel Profile picture" style={imageStyle}/>);
+        }
+        else{
+            return ( <img src={channelDp} className="channel-icon" alt="Channel Profile picture" style={imageStyle}/>);                          
+        }
+    }
+
+    function Price () {
+        if (props.price>0) {
+            return (<p style={priceStyle}>KShs. { props.price }</p>);
+        }else{
+            return (<></>);
+        }
+    }
+    
     return (
         <>
             <div className="col-lg-4">
@@ -63,23 +82,17 @@ function Videocard (props: Prop) {
                         <div className="col-lg-6">
                             <div className="row">
                                 <a href="#">
-                                {/* {% if video.channel.profile_picture %} */}
-                                <img src={channelDp} className="channel-icon" alt="Channel Profile picture" style={imageStyle}/>
-                                {/* {% else %} */}
-                                {/* <img src={imagePlaceholder} className="channel-icon" alt="Channel Profile picture" style={imageStyle}/> */}
-                                {/* {% endif %} */}
+                                    <Thumbnail/>
                                 </a>
                                 <a href="#">
                                     <h6>{videoChannelName}</h6>
                                 </a>
                             </div>
                         </div>
-                        {/* {% if video.price > 0 %} */}
                         <div className="col-lg-6 col-12 text-right mt-2 mt-lg-0">
                             {/* <p style={priceStyle}>KShs. {{ video.price | floatformat:2 | intcomma }}</p> */}
-                            <p style={priceStyle}>KShs. { props.price }</p>
+                            <Price/>
                         </div>
-                        {/* {% endif %} */}
                     </div>
                     <div className="row">
                         <div className="col-lg-10">
