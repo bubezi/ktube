220c42982dd675d0f2e4e34a0aef4d07713db78c
Blacking-highness <pbubezi@gmail.com>
2023-07-16 07:19:31 +0300

    update

--- ktube/ktube-react/src/providers/ViewerProvider.tsx
+++ ktube/ktube-react/src/providers/ViewerProvider.tsx
@@ -1,14 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from "axios";
+import { API_URL } from "../constants";
 
-const ViewerContext = React.createContext();
+interface Viewer {
+    id: number,
+    username: string,
+    phone: number,
+    gender: string,
+    joined: number,
+    wallet: number,
+}
 
-const Viewer = {id:0, username:"", phone:0, gender:"", joined:0, wallet:0};
+const defaultValue: unknown = '';
+
+const ViewerContext = React.createContext(defaultValue);
+
+const viewerState :Viewer = {id:0, username:"", phone:0, gender:"", joined:0, wallet:0};
+
 
 const ViewerProvider = ({ children }) => {
-    const [viewer, setViewer] = React.useState(Viewer);
+    const [viewer, setViewer] = React.useState(viewerState);
+
+    React.useEffect(()=>{
+        axios.get(API_URL+'auth/getCurrentViewer')
+            .then(res=> setViewer(res.data))
+            .catch((error)=>{console.log(error)});
+    }, []);
     
     return (
-        <ViewerContext.Provider value={{ viewer, setViewer }}>
+        <ViewerContext.Provider value={{ viewer }}>
             {children}
         </ViewerContext.Provider>
     );
