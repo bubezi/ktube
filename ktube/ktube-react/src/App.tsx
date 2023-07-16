f02c7edf28104040251514521d63b1b11841d485
Blacking-highness <pbubezi@gmail.com>
2023-07-16 18:22:24 +0300

    update

--- ktube/ktube-react/src/App.tsx
+++ ktube/ktube-react/src/App.tsx
@@ -1,43 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Login from './components/Login';
-import Preferences from './components/Preferences/Preferences';
-import Loader from './components/Loader';
+// import './App.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import { Routes, Route } from "react-router-dom";
+
+import Homepage from './components/Homepage';
+import Loader from "./components/Loader";
+// import Watchpage from "./components/Watchpage";
+
 
 function App() {
-  const [token, setToken] = React.useState (() => {
-    // Try to load the token from local storage
-    const savedToken = localStorage.getItem('token');
-    return savedToken ?? null;
-  });
-
-  const saveToken = (userToken: React.SetStateAction<string | null>) => {
-    localStorage.setItem('token', String(userToken));
-    setToken(userToken);
-  };
-  
-  if(!token) {
-    return (
-        <>
-          <Loader/>
-          <Login saveToken={saveToken} />
-        </>
-          );
-  }
 
   return (
-    <div className="wrapper">
-      <Loader/>
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preferences" element={<Preferences />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    <>
+      <Loader />
+      <Homepage/>
+    </>
+  )
 }
-export default App;
+
+export default App
